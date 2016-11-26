19889fa31ca6e991f96e2f0fcb20d0b3329e3505
guven <guvennaslan@gmail.com>
2016-11-26 02:44:29 +0200

    Completed

---
+++ ZipFile.Example/ZipFile.Example/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ZipFile.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testFilePath = @"D:\TestFiles";
+            string zipFilePath = @"D:\TestZipFiles\files.zip";
+            string extractPath = @"D:\Files\extract";
+
+            System.IO.Compression.ZipFile.CreateFromDirectory(testFilePath, zipFilePath);
+
+            System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath, extractPath);
+
+            Console.ReadKey();
+        }
+    }
+}
